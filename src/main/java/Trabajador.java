8de4c0a01d8d240b997408be1f0751988905412a
git status <iarribanav@educacion.navarra.es>
2025-12-16 09:32:41 +0100

    prueba unitaria

--- src/main/java/Trabajador.java
+++ src/main/java/Trabajador.java
@@ -40,7 +40,11 @@ public class Trabajador {
         // 10 espacios para el nombre alineado a la izquierda seguido de su precio hora espacio y "E." utilizando format
         // 123456789012345678901234567890
         // Aitor     90 E.
-        return String.format("%-8s% %3d E.",nombre,getPrecioHora());
+        return String.format("%-8s %3d E.",nombre,getPrecioHora());
     }
 
+    public static void main(String[] args) {
+        Trabajador t1 = new Trabajador("Jaime");
+        System.out.println(t1);
+    }
 }
